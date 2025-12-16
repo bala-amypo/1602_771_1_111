2e560665d2ed1ea6e1e4e3933c6f52f03577da57
bala-amypo <bala@amypo.in>
2025-12-16 04:03:55 +0000

    User commit

--- src/main/java/com/example/demo/model/HabitProfile.java
+++ src/main/java/com/example/demo/model/HabitProfile.java
@@ -1,5 +1,13 @@
 package com.example.demo.model;
 
 public class HabitProfile {
-    
+    private long id;
+    private long studentId;
+    private String sleepSchedule;
+    private int studyHoursPerDay;
+    private String cleanlinessLevel;
+    private String noiseTolerance
+    private String socialPreference;
+    private LocalDateTime updatedAt;
 }
+public HabitProfile(){}
