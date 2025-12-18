3d043ca5be9323cef91b38ca0b866e8f8099dbb5
bala-amypo <bala@amypo.in>
2025-12-18 15:54:18 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/HabitEnums.java
@@ -0,0 +1,26 @@
+package com.example.demo.model;
+
+public class HabitEnums {
+
+    public enum SleepSchedule {
+        EARLY_BIRD,
+        NIGHT_OWL
+    }
+
+    public enum CleanlinessLevel {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+
+    public enum NoiseTolerance {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+
+    public enum SocialPreference {
+        INTROVERT,
+        EXTROVERT
+    }
+}
