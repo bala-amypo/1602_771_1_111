de0d725421abce49df4e80733de97797ba407e6d
bala-amypo <bala@amypo.in>
2025-12-19 04:22:10 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/StudentProfileServiceImpl.java
@@ -0,0 +1,58 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.StudentProfile;
+import com.example.demo.repository.StudentProfileRepository;
+import com.example.demo.service.StudentProfileService;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class StudentProfileServiceImpl implements StudentProfileService {
+
+    private final StudentProfileRepository studentProfileRepository;
+
+    // Constructor Injection (MANDATORY)
+    public StudentProfileServiceImpl(StudentProfileRepository studentProfileRepository) {
+        this.studentProfileRepository = studentProfileRepository;
+    }
+
+    @Override
+    public StudentProfile createStudent(StudentProfile profile) {
+
+        if (studentProfileRepository.findByStudentId(profile.getStudentId()).isPresent()) {
+            throw new IllegalArgumentException("studentId exists");
+        }
+
+        profile.setCreatedAt(LocalDateTime.now());
+        profile.setActive(true);
+
+        return studentProfileRepository.save(profile);
+    }
+
+    @Override
+    public StudentProfile getStudentById(Long id) {
+        return studentProfileRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("not found"));
+    }
+
+    @Override
+    public List<StudentProfile> getAllStudents() {
+        return studentProfileRepository.findAll();
+    }
+
+    @Override
+    public StudentProfile findByStudentId(String studentId) {
+        return studentProfileRepository.findByStudentId(studentId)
+                .orElseThrow(() -> new ResourceNotFoundException("not found"));
+    }
+
+    @Override
+    public StudentProfile updateStudentStatus(Long id, boolean active) {
+        StudentProfile student = getStudentById(id);
+        student.setActive(active);
+        return studentProfileRepository.save(student);
+    }
+}
