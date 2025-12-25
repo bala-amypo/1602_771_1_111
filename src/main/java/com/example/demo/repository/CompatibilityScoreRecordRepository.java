b4c1d0c5abd86f801f4d8415f498d326f518dc60
bala-amypo <bala@amypo.in>
2025-12-25 04:04:40 +0000

    User commit

--- src/main/java/com/example/demo/repository/CompatibilityScoreRecordRepository.java
+++ src/main/java/com/example/demo/repository/CompatibilityScoreRecordRepository.java
@@ -13,16 +13,4 @@ public interface CompatibilityScoreRecordRepository
 
     List<CompatibilityScoreRecord> findByStudentAIdOrStudentBId(Long id1, Long id2);
 }
- this is CompatibilityScoreRecordRepository.java
-
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.HabitProfile;
-import java.util.Optional;
-
-public interface HabitProfileRepository extends JpaRepository<HabitProfile, Long> {
-
-    Optional<HabitProfile> findByStudentId(Long studentId);
-}
- thsi is 
+ 
